43ec1237cb887cb7d564374b50894f9b3f99f190
Andrew Couch <hi@andrewcou.ch>
2017-11-03 21:51:36 -0400

    clean up datumFromState implementation

--- src/datumFromState.js
+++ src/datumFromState.js
@@ -1,10 +1,8 @@
-import { select } from 'd3-selection';
 import storeLocal from './local';
 
 export default function (selector) {
-  return this.each(function () {
+  return this.datum(function () {
     var store = storeLocal.get(this);
-    select(this)
-      .datum(selector.call(this, store.getState()));
+    return selector.call(this, store.getState());
   });
 }
