e706d670fb38008bb8c64c7994b776953c811e2d
Andrew Couch <hi@andrewcou.ch>
2017-11-08 23:57:10 -0500

    rollup config

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+export default {
+  input: "index.js",
+  output: {
+    file: "build/d3-redux.js",
+    format: "umd",
+    name: "d3",
+    globals: {
+      "d3-selection": "d3"
+    },
+    sourcemap: true
+  }
+};
